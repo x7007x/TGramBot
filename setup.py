d99798c5968d63913bebb6b23f086fbd01f3241b
x7007x <160288929+x7007x@users.noreply.github.com>
2024-11-07 16:12:32 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TGramBot",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # ضع هنا أي مكتبات تعتمد عليها حزمةك
+    ],
+    author="Ahmed Negm",
+    author_email="a7mednegm.x@gmail.com",
+    description="None",
+    url="https://github.com/x7007x/TGramBot",
+)
